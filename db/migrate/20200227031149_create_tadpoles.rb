1be72639cfd980e1dff2513dc258889f4fdb773f
Sylvia <sylviapap1@gmail.com>
2020-02-26 20:13:53 -0800

    Done.

---
+++ db/migrate/20200227031149_create_tadpoles.rb
@@ -0,0 +1,9 @@
+class CreateTadpoles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tadpoles do |t|
+      t.string :name
+      t.string :color
+      t.references :frog, foreign_key: true
+    end
+  end
+end
